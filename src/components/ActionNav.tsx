aa444e6f82858596ff9ae896c6c36772ffd4c806
elvis-ndubuisi <provictor.ie@gmail.com>
2023-02-25 13:22:33 +0100

    Dashboard Nav update

--- src/components/ActionNav.tsx
+++ src/components/ActionNav.tsx
@@ -3,22 +3,44 @@ import { NavLink } from "react-router-dom";
 const ActionNav = () => {
   return (
     <nav className="flex items-center gap-3 flex-wrap max-w-5xl mx-auto px-1 border-b border-b-black py-2">
-      <NavLink to="" className="font-semibold hover:text-polar-green-300">
+      <NavLink
+        to="/dashboard"
+        className={({ isActive }) =>
+          isActive
+            ? "font-semibold hover:text-polar-green-300 text-polar-green-300"
+            : "font-semibold hover:text-polar-green-300"
+        }
+      >
         Home
       </NavLink>
       <NavLink
         to="proverbs"
-        className="font-semibold hover:text-polar-green-300"
+        className={({ isActive }) =>
+          isActive
+            ? "font-semibold hover:text-polar-green-300 text-polar-green-300"
+            : "font-semibold hover:text-polar-green-300"
+        }
       >
         Proverbs
       </NavLink>
       <NavLink
         to="submitted"
-        className="font-semibold hover:text-polar-green-300"
+        className={({ isActive }) =>
+          isActive
+            ? "font-semibold hover:text-polar-green-300 text-polar-green-300"
+            : "font-semibold hover:text-polar-green-300"
+        }
       >
         Submits
       </NavLink>
-      <NavLink to="admins" className="font-semibold hover:text-polar-green-300">
+      <NavLink
+        to="admins"
+        className={({ isActive }) =>
+          isActive
+            ? "font-semibold hover:text-polar-green-300 text-polar-green-300"
+            : "font-semibold hover:text-polar-green-300"
+        }
+      >
         Admins
       </NavLink>
     </nav>
