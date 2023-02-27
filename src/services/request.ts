d6cb60254ef9d7715921ba0e93d106d4baab6330
elvis-ndubuisi <provictor.ie@gmail.com>
2023-02-27 18:53:35 +0100

    clean-up -- request.ts

--- src/services/request.ts
+++ src/services/request.ts
@@ -21,34 +21,3 @@ request.interceptors.response.use(
 );
 
 export default request;
-
-// instance.interceptors.response.use(
-//   (res) => {
-//     return res;
-//   },
-//   async (err) => {
-//     const originalConfig = err.config;
-
-//     if (originalConfig.url !== "/auth/signin" && err.response) {
-//       // Access Token was expired
-//       if (err.response.status === 401 && !originalConfig._retry) {
-//         originalConfig._retry = true;
-
-//         try {
-//           const rs = await instance.post("/auth/refreshtoken", {
-//             refreshToken: TokenService.getLocalRefreshToken(),
-//           });
-
-//           const { accessToken } = rs.data;
-//           TokenService.updateLocalAccessToken(accessToken);
-
-//           return instance(originalConfig);
-//         } catch (_error) {
-//           return Promise.reject(_error);
-//         }
-//       }
-//     }
-
-//     return Promise.reject(err);
-//   }
-// );
